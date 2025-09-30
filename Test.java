6d419033a6cfd79e4697ea2632d57b49b8e46267
bhuvanhb <bhuvanhbbsc24@rvu.edu.in>
2025-09-30 10:58:24 +0530

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import P1.A;   // importing class A from package P1
-
-public class Test {
-    public static void main(String[] args) {
-        A obj = new A();
-        obj.show();
-        
-    }
-}
